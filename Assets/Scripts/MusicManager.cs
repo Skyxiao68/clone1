4bf8ca91086219176e4fbabb5d364ad79a13a0a3
Za <Zubuhle01@gmail.com>
2026-08-04 11:45:49 +0200

    soun from the legend of Zelda 1986

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicManager : MonoBehaviour
 {
@@ -40,4 +41,19 @@ public class MusicManager : MonoBehaviour
         PlayerPrefs.SetFloat("MasterVolume", volume);
         PlayerPrefs.Save();
     }
+
+    public void ToggleMute()
+    {
+        if (AudioListener.volume > 0)
+        {
+            AudioListener.volume = 0;
+            volumeSlider.value = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            volumeSlider.value = 1;
+        }
+    }
+
 }
