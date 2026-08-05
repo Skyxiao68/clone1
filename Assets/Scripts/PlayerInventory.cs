df0f59e75c4e90ef3eec93f3dd8a22e9616e1cef
Za <Zubuhle01@gmail.com>
2026-08-05 06:58:15 +0200

    Added the bomb spawn successfully now I have to make the bomb expload when the enemy collides with it

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -7,14 +7,29 @@ public class PlayerInventory : MonoBehaviour
 
     public Transform bombSpawnPoint;
 
-    public void OnUseBomb(InputAction.CallbackContext context)
+    /* public void OnUseBomb(InputAction.CallbackContext context)
+     {
+         //Debug.Log("E pressed");
+         if (!context.performed)
+             return;
+         Debug.Log("E pressed");
+
+         //  console.log("I punched e");
+
+         if (GameManager.Instance.UseBomb())
+         {
+             Instantiate(bombPrefab,
+                         bombSpawnPoint.position,
+                         Quaternion.identity);
+         }
+     }*/
+
+    public void UseBomb(InputAction.CallbackContext context)
     {
-        //Debug.Log("E pressed");
+        Debug.Log("E Pressed");
+
         if (!context.performed)
             return;
-        Debug.Log("E pressed");
-
-        //  console.log("I punched e");
 
         if (GameManager.Instance.UseBomb())
         {
